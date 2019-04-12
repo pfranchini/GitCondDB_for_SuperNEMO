11fd348feefc15e7bcf4eb22daff9608aa3c147e
Paolo Franchini <pfranchini@gmail.com>
2019-04-13 00:43:17 +0100

    Improved read_gitconddb

--- src/utilities/read_gitconddb.cpp
+++ src/utilities/read_gitconddb.cpp
@@ -1,22 +1,53 @@
+/******************************************************************************
+*
+*  read_gitconddb
+*  ==============
+*   - Print a condition for a given repository, source, tag and time
+*   - Clone a list of conditions for a given repository, source and tag
+* 
+*  P. Franchini 2019 for SuperNEMO
+*
+\******************************************************************************/
+
 #include "GitCondDB.h"
+#include "DBImpl.h"
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
 #include "DBImpl.h"
 #include <getopt.h>
+#include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <sys/stat.h> 
+#include <sys/types.h> 
+
+using json = nlohmann::json;
 
 void print_usage() {
-  printf("Usage: read_gitconddb -r <[file|git]:local_repository_path> -s <source> -c <condition> -t <time>\n\n");
+  printf("Usage: read_gitconddb -v <tag> -r <[file|git]:local_repository_path> -s <source> (-c <condition>) (-t <time>) \n\n");
+  // omitting file|git: corresponds to git:
+  // should probably avoid using file: because uses the local checkout, whatever tag it is, so the -v tag is not really considered
 }
 
 int main(int argc, char **argv) {
 
+  // Cache directory (can also be an env variable)
+  const char* HOME = std::getenv("HOME");
+  std::string CACHE_DIR = ".cache/snemo";
+
+  const char *tag = "HEAD";
   char *repository = NULL;
   char *source = NULL;
   char *condition = NULL;
-  long unsigned int time = 0;  
+  long unsigned int time = 0;
+  
   int option_index = 0;
-
-  while (( option_index = getopt(argc, argv, "r:s:c:t:")) != -1){
+  while (( option_index = getopt(argc, argv, "v:r:s:c:t:")) != -1){
     switch (option_index) {
+    case 'v':
+      tag = optarg;
+      break;
     case 'r':
       repository = optarg;
       break;
@@ -29,9 +60,6 @@ int main(int argc, char **argv) {
     case 't':
       time = atof(optarg);
       break;
-      //    case '?':
-      //      if (optopt == 'r')
-      //	fprintf (stderr, "Option -%d requires an argument.\n", optopt);
     default:
       printf("Option incorrect\n");
       print_usage();
@@ -40,21 +68,71 @@ int main(int argc, char **argv) {
   }  
 
   std::cout << std::endl << "Repository: " << repository << std::endl;
+  std::cout << "Tag: " << tag << std::endl;
   std::cout << "Source: " << source << std::endl;
-  std::cout << "Condition: " << condition << std::endl;
+  if (condition) std::cout << "Condition: " << condition << std::endl;
   std::cout << "Time: " << time << std::endl << std::endl;
-  
-  //  auto db = GitCondDB::connect( "git:path/to/repo.git" );
-  //  auto db = GitCondDB::connect( "test_data/repo.git" );
-  //  auto db = GitCondDB::connect( "file:test_data/repo" );
-  //  auto db = GitCondDB::connect( "file:/home/hep/pfranchi/SuperNEMO/supernemo_test_cdb.git" );
 
-  auto db = GitCondDB::connect( repository );
-  GitCondDB::CondDB::Key key{"HEAD",strcat(strcat(source,"/"),condition),time};
-  auto cond = db.get(key);
+  char path[80];
+  strcpy(path, source);
+  if (condition) {
+    strcat(path, "/");
+    strcat(path, condition);
+  }
   
-  std::cout << "Data:\n" << std::get<0>( cond ) << std::endl;
-  std::cout << "IOV: [" <<  std::get<1>( cond ).since << ", " << std::get<1>( cond ).until << ")\n\n";
+  // Print condition and IOV for the given repository, source, (condition), tag and (time)
+  auto db = GitCondDB::connect( repository );
+  GitCondDB::CondDB::Key key{tag,path,time};
+  auto cond = db.get(key);
+  std::cout << "Data:\n" << std::get<0>( cond ) << std::endl << std::endl;
+  if (time) std::cout << "IOV: [" <<  std::get<1>( cond ).since << ", " << std::get<1>( cond ).until << ")\n\n";
+  
+  // Clone the full directory for a given repository, source and tag
+  if (!condition){
+
+    // Create/read a json file and write each file on the cache
+    std::ofstream json_file;
+    json_file.open ("repo.json");
+    json_file << std::get<0>( cond );
+    json_file.close();
+    json j;
+    std::ifstream i("repo.json");
+    i >> j;
+
+    for (auto itr : j["files"]) {
+
+      struct stat st;
+      
+      // Read each single file 
+      std::string file =  itr;  
+      strcpy(path, source); 
+      strcat(path, "/"); 
+      strcat(path, file.c_str());
+      auto [file_content, iov] = db.get( {tag, path, time} );  
+
+      // Create directory for tag/
+      char full_path[80];
+      strcpy(full_path,HOME);
+      strcat(full_path,"/");
+      strcat(full_path,CACHE_DIR.c_str());
+      strcat(full_path,"/");
+      strcat(full_path,tag);
+      if (stat(full_path, &st) == -1) mkdir(full_path,0755); 
+      
+      // Create directory for source/
+      strcat(full_path,"/");
+      strcat(full_path,source);
+      if (stat(full_path, &st) == -1) mkdir(full_path,0755); 
+
+      // Write file in the cache directory
+      strcat(full_path,"/");
+      strcat(full_path,file.c_str());
+      std::cout << full_path << std::endl;
+      std::ofstream f(full_path);
+      f << file_content;
+      
+    }//for
+  }//if
   
   return 0;
 
